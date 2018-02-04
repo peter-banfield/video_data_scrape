1793e3a00934d5965deb028c820c749652fcc225
Peter Banfield <peter.e.banfield@gmail.com>
2018-02-04 00:25:37 -0600

    fixed generation of disney list

--- scrape.py
+++ scrape.py
@@ -38,9 +38,9 @@ with open('tv.txt', 'w') as output:
             showInfo['studio'] = show.find('studio').text
         except:
             showInfo['studio'] = ' '
-        output.write('{}\t{}\t{}\t{}\n'.format(movieInfo['title'], movieInfo['genre'], movieInfo['year'], movieInfo['studio']))
+        output.write('{}\t{}\t{}\t{}\n'.format(showInfo['title'], showInfo['genre'], showInfo['year'], showInfo['studio']))
 
-with open('disney.csv', 'w') as output:
+with open('disney.txt', 'w') as output:
     # output the header row
     output.write('Title\tGenre\tYear\tStudio\n')
     # create dictionary with all disney movies titles as keys and the associated year as the value
@@ -48,17 +48,18 @@ with open('disney.csv', 'w') as output:
         inFile.readline()
         disneyList = {}
         for line in inFile:
+            line = line.strip()
             values = line.split('\t')
             disneyList[values[0]] = values[1]
     # for tv show tag find the title genre year and studio then write the row to the file    
     for movie in root.findall('movie'):
-        if movie.find('title').text in list(disneyList.keys()) and movie.find('year').text == disneyList[movie.find('title').text]:
-            movieInfo = {}
-            movieInfo['title'] = movie.find('title').text
-            movieInfo['genre'] = [item.text for item in movie.findall('genre')]
-            movieInfo['year'] = movie.find('year').text
-            try:
-                movieInfo['studio'] = movie.find('studio').text
-            except:
-                movieInfo['studio'] = ' '
-            output.write('{}\t{}\t{}\t{}\n'.format(movieInfo['title'], movieInfo['genre'], movieInfo['year'], movieInfo['studio']))
+        disneyInfo = {}
+        disneyInfo['title'] = movie.find('title').text
+        disneyInfo['genre'] = [item.text for item in movie.findall('genre')]
+        disneyInfo['year'] = movie.find('year').text
+        try:
+            disneyInfo['studio'] = movie.find('studio').text
+        except:
+            disneyInfo['studio'] = ' '
+        if disneyInfo['title'] in list(disneyList.keys()) and disneyInfo['year'] == disneyList[disneyInfo['title']]:
+            output.write('{}\t{}\t{}\t{}\n'.format(disneyInfo['title'], disneyInfo['genre'], disneyInfo['year'], disneyInfo['studio']))
